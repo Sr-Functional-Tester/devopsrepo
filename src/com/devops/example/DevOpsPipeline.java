0d05a16e8abbe0e4486c1803a394b4e66f0728db
srinivasarao <srinivas.seniordeveloper@gmail.com>
2025-07-07 08:10:02 -0400

    Add webhook testing System.out.println statements

--- src/com/devops/example/DevOpsPipeline.java
+++ src/com/devops/example/DevOpsPipeline.java
@@ -4,6 +4,9 @@ public class DevOpsPipeline {
 	
 	public static void main(String args[]) {
 		System.out.println("devops pipeline example");
+		System.out.println("Testing GitHub webhook integration");
+		System.out.println("Webhook listener should detect this change");
+		System.out.println("Current timestamp: " + System.currentTimeMillis());
 	}
 
 }
